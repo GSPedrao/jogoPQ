5ee211a6df0be5d17b9ff3873714ad9d7273cf35
GABRIEL SOUZA PEDRAO <gabrielsouzap@hotmail.com.br>
2021-09-01 15:01:17 -0300

    Quase finaliza

--- Assets/Scenes/dragonbones/Knight.cs
+++ Assets/Scenes/dragonbones/Knight.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Knight : MonoBehaviour
 {
@@ -9,10 +10,19 @@ public class Knight : MonoBehaviour
     public float tempoPulo;
     private float tempoPulado;
     private Vector3 posicaoIni;
+    private Animator anim;
+    private SpriteRenderer sprite;
+    public GameObject textoQtdClube;
+    private int qtdClube;
+
     // Start is called before the first frame update
     void Start()
     {
-        posicaoIni = this.transform.position; 
+        posicaoIni = this.transform.position;
+        anim = this.GetComponent<Animator>();
+        sprite = this.GetComponent<SpriteRenderer>(); 
+        qtdClube = 0;
+        AtualizarHUD();
     }
 
     // Update is called once per frame
@@ -20,12 +30,14 @@ public class Knight : MonoBehaviour
     {
         if(Input.GetKey(KeyCode.D))
         {
+             sprite.flipX = false;
             Vector3 pos = this.transform.position;
             pos.x +=  velocidade;
             this.transform.position = pos;
         }
         if(Input.GetKey(KeyCode.A))
         {
+              sprite.flipX = true;
              Vector3 pos = this.transform.position;
              pos.x -= velocidade;
              this.transform.position = pos;
@@ -37,6 +49,7 @@ public class Knight : MonoBehaviour
              Vector2 forca = new  Vector2(0f, 7f);
              rb.AddForce(forca, ForceMode2D.Impulse);
              tempoPulado = tempoPulo;
+             anim.SetBool("estaPulando", true);
         }
         tempoPulado -= Time.deltaTime;
         
@@ -44,15 +57,42 @@ public class Knight : MonoBehaviour
         {
             this.transform.position = posicaoIni;
         }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))
+        {
+            anim.SetBool("andando", true);
+        }
+        else
+        {
+            anim.SetBool("andando", false);
+        }
     }
     
 
     //-----------------------//
     void OnCollisionEnter2D(Collision2D col)
     {
+         if(col.gameObject.tag == "spike")
+         {
+             this.transform.position = posicaoIni;
+         }
+         if(col.gameObject.tag == "chao")
+        {
+            anim.SetBool("estaPulando", false);
+        }
+        if(col.gameObject.tag == "clubeSocial")
+        {
+            qtdClube++;
+            AtualizarHUD();
+            Destroy(col.gameObject);
+        }   
 
+    
+    }
+            void AtualizarHUD(){
+            textoQtdClube.GetComponent<Text>().text = qtdClube.ToString();
+        }
        
         
         
-    }
+    
 }
